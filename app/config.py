75c33a7ff298e9c62aafa6c57974deb7e5ff0109
TeacherOp <admin@teacherop.com>
2024-10-04 12:30:06 +0530

    use env

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
 # File: app/config.py
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SUPABASE_URL = "https://zdmttxqybtfifynjlzgl.supabase.co"
-    SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkbXR0eHF5YnRmaWZ5bmpsemdsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNzc3MjUwNywiZXhwIjoyMDQzMzQ4NTA3fQ.8VUchuT4kT-Zv0A3T2OkKM9IZzCRPnh28D6B3SA7E5w"
+    SUPABASE_URL = os.environ.get('SUPABASE_URL')
+    SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
